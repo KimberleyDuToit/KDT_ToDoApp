2157e15a3eca9d52c67dc58fbb06b4e935df8bf5
KimberleyDuToit <kimberley.anne.watson1@gmail.com>
2021-08-06 15:12:08 +0200

    Function: add list items
    User can now add items to unordered list via the field.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-<script>
-    
-</script>
+// Add a new item to the list
+function newToDo (){
+    let li = document.createElement("li");
+    let taskInfo = document.getElementById("taskName").value;
+    let task = document.createTextNode(taskInfo);
+        li.appendChild (task);
+        if (taskInfo === ''){
+            alert ("Please enter a task name.");
+        } //in case field is empty upon add
+        else {
+            document.getElementById("taskList").appendChild(li);
+        }
+        document.getElementById("taskName").value = "";
+}
